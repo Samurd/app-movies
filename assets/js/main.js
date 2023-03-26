44400f2cb401257c83623e606f29c1d025b31cbd
Samuel Viloria <samuelviloriadd@gmail.com>
2023-03-26 17:43:41 -0500

    agregamos seccion de ultimas peliculas

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,12 @@ const slides = document.querySelectorAll(".slide")
 const imgsSlide = document.querySelectorAll(".img-slide")
 const titles = document.querySelectorAll(".title")
 const descs = document.querySelectorAll(".description")
+const description = document.querySelector(".description")
 const btnR = document.querySelector(".btnR button")
 const btnL = document.querySelector(".btnL button")
+const btnLLatest = document.querySelector(".btnL-latest button")
+const btnRLatest = document.querySelector(".btnR-latest button")
+const containerUpComing = document.querySelector(".container-upcoming")
 let current = 0;
 
 
@@ -33,11 +37,56 @@ async function callApiRenderMainMovies () {
 
 callApiRenderMainMovies();
 
+
+async function callApiRenderUpComingMovies () {
+    try {
+        const response = await fetch("https://api.themoviedb.org/3/movie/upcoming?api_key=3fd98058cfc374cfff84bc470ea77e90")
+        console.log(response)
+        const data = await response.json();
+        const dataResults = await data.results
+        console.log(dataResults)
+        let moviesUpcoming = ''
+        dataResults.forEach(movie => {
+            moviesUpcoming = moviesUpcoming + `<div class="card-upcoming">
+            <img class="img-card-upcoming" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="">
+            <h3 class="title-upcoming">${movie.title}</h3>
+        </div>`
+        });
+
+        containerUpComing.innerHTML = moviesUpcoming;
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+callApiRenderUpComingMovies()
+
+
+btnLLatest.addEventListener("click", latestScrollLeft)
+
+function latestScrollLeft() {
+
+    containerUpComing.scrollLeft -= document.querySelector(".img-card-upcoming").offsetWidth + 12;
+
+}
+
+btnRLatest.addEventListener("click", latestScrollRight)
+
+function latestScrollRight() {
+
+    containerUpComing.scrollLeft += document.querySelector(".img-card-upcoming").offsetWidth + 12;
+}
+
 function cls() {
     for (let i = 0; i < slides.length; i++){
         slides[i].style.display = 'none';
     }
 }
+
+setInterval(() => {
+    next()
+}, 5000)
 
 btnR.addEventListener("click", next);
 
